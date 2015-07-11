7e0e18d8335cf5eee6a162ea7981ad02ca4294b2
Andrew Naylor <argon@andrews-macbook-pro.local>
2015-07-11 11:30:30 +0100

    Initial Commit

---
+++ mas-cli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mas-cli
+//
+//  Created by Andrew Naylor on 11/07/2015.
+//  Copyright © 2015 Andrew Naylor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
