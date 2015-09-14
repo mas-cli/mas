6ef4d842ed0e8066cc8be9b6740f6ca8703fcb0b
Andrew Naylor <argon@mkbot.net>
2015-09-14 09:26:03 +0100

    Remove unnecessary headers

--- mas-cli/Headers/CommerceKit/ISStoreURLOperationDelegate-Protocol.h
+++ mas-cli/Headers/CommerceKit/ISStoreURLOperationDelegate-Protocol.h
@@ -4,7 +4,7 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import "ISURLOperationDelegate-Protocol.h"
+#import <StoreFoundation/ISURLOperationDelegate-Protocol.h>
 
 @class ISStoreURLOperation, NSNumber, NSString;
 
--- mas-cli/Headers/StoreFoundation/ISOperationDelegate-Protocol.h
+++ mas-cli/Headers/StoreFoundation/ISOperationDelegate-Protocol.h
@@ -4,8 +4,6 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import "NSObject.h"
-
 @class ISOperation, NSError, SSOperationProgress;
 
 @protocol ISOperationDelegate <NSObject>
