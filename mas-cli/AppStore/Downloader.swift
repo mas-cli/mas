490c73c6acb413a3811230dff938574c340efe57
Andrew Naylor <argon@mkbot.net>
2016-02-14 01:24:30 +0000

    Use a guard instead to make it tidier

--- mas-cli/AppStore/Downloader.swift
+++ mas-cli/AppStore/Downloader.swift
@@ -8,45 +8,44 @@
 
 func download(adamId: UInt64) -> MASError? {
 
-    if let account = ISStoreAccount.primaryAccount {
-        let group = dispatch_group_create()
-        let purchase = SSPurchase(adamId: adamId, account: account)
-        
-        var purchaseError: MASError?
-        
-        purchase.perform { purchase, unused, error, response in
-            if let error = error {
-                purchaseError = MASError(code: .PurchaseError, sourceError: error)
-                dispatch_group_leave(group)
-                return
-            }
-            
-            if let downloads = response.downloads where downloads.count > 0 {
-                let observer = PurchaseDownloadObserver(purchase: purchase)
-                
-                observer.errorHandler = { error in
-                    purchaseError = error
-                    dispatch_group_leave(group)
-                }
-                
-                observer.completionHandler = {
-                    dispatch_group_leave(group)
-                }
-                
-                CKDownloadQueue.sharedDownloadQueue().addObserver(observer)
-            }
-            else {
-                print("No downloads")
-                purchaseError = MASError(code: .NoDownloads)
-                dispatch_group_leave(group)
-            }
-        }
-        
-        dispatch_group_enter(group)
-        dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
-        return purchaseError
-    }
-    else {
+    guard let account = ISStoreAccount.primaryAccount else {
         return MASError(code: .NotSignedIn)
     }
+    
+    let group = dispatch_group_create()
+    let purchase = SSPurchase(adamId: adamId, account: account)
+    
+    var purchaseError: MASError?
+    
+    purchase.perform { purchase, unused, error, response in
+        if let error = error {
+            purchaseError = MASError(code: .PurchaseError, sourceError: error)
+            dispatch_group_leave(group)
+            return
+        }
+        
+        if let downloads = response.downloads where downloads.count > 0 {
+            let observer = PurchaseDownloadObserver(purchase: purchase)
+            
+            observer.errorHandler = { error in
+                purchaseError = error
+                dispatch_group_leave(group)
+            }
+            
+            observer.completionHandler = {
+                dispatch_group_leave(group)
+            }
+            
+            CKDownloadQueue.sharedDownloadQueue().addObserver(observer)
+        }
+        else {
+            print("No downloads")
+            purchaseError = MASError(code: .NoDownloads)
+            dispatch_group_leave(group)
+        }
+    }
+    
+    dispatch_group_enter(group)
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+    return purchaseError
 }
