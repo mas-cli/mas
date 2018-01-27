f4c34f77b503f0e74419df2acf2a2288b406041d
Ben Chatelain <ben@octop.ad>
2018-01-26 19:32:30 -0700

    🌱 Update seeds
    - Commandant 0.12.0+ (Swift 4 fixes in master)
    - Result 3.2.4

--- Seeds/Commandant/Sources/Commandant/Argument.swift
+++ Seeds/Commandant/Sources/Commandant/Argument.swift
@@ -30,66 +30,70 @@ public struct Argument<T> {
 	}
 }
 
-/// Evaluates the given argument in the given mode.
-///
-/// If parsing command line arguments, and no value was specified on the command
-/// line, the argument's `defaultValue` is used.
-public func <| <T: ArgumentProtocol, ClientError>(mode: CommandMode, argument: Argument<T>) -> Result<T, CommandantError<ClientError>> {
-	switch mode {
-	case let .arguments(arguments):
-		guard let stringValue = arguments.consumePositionalArgument() else {
-			if let defaultValue = argument.defaultValue {
-				return .success(defaultValue)
-			} else {
-				return .failure(missingArgumentError(argument.usage))
+// MARK: - Operators
+
+extension CommandMode {
+	/// Evaluates the given argument in the given mode.
+	///
+	/// If parsing command line arguments, and no value was specified on the command
+	/// line, the argument's `defaultValue` is used.
+	public static func <| <T: ArgumentProtocol, ClientError>(mode: CommandMode, argument: Argument<T>) -> Result<T, CommandantError<ClientError>> {
+		switch mode {
+		case let .arguments(arguments):
+			guard let stringValue = arguments.consumePositionalArgument() else {
+				if let defaultValue = argument.defaultValue {
+					return .success(defaultValue)
+				} else {
+					return .failure(missingArgumentError(argument.usage))
+				}
 			}
-		}
 
-		if let value = T.from(string: stringValue) {
-			return .success(value)
-		} else {
-			return .failure(argument.invalidUsageError(stringValue))
-		}
+			if let value = T.from(string: stringValue) {
+				return .success(value)
+			} else {
+				return .failure(argument.invalidUsageError(stringValue))
+			}
 
-	case .usage:
-		return .failure(informativeUsageError(argument))
+		case .usage:
+			return .failure(informativeUsageError(argument))
+		}
 	}
-}
 
-/// Evaluates the given argument list in the given mode.
-///
-/// If parsing command line arguments, and no value was specified on the command
-/// line, the argument's `defaultValue` is used.
-public func <| <T: ArgumentProtocol, ClientError>(mode: CommandMode, argument: Argument<[T]>) -> Result<[T], CommandantError<ClientError>> {
-	switch mode {
-	case let .arguments(arguments):
-		guard let firstValue = arguments.consumePositionalArgument() else {
-			if let defaultValue = argument.defaultValue {
-				return .success(defaultValue)
-			} else {
-				return .failure(missingArgumentError(argument.usage))
+	/// Evaluates the given argument list in the given mode.
+	///
+	/// If parsing command line arguments, and no value was specified on the command
+	/// line, the argument's `defaultValue` is used.
+	public static func <| <T: ArgumentProtocol, ClientError>(mode: CommandMode, argument: Argument<[T]>) -> Result<[T], CommandantError<ClientError>> {
+		switch mode {
+		case let .arguments(arguments):
+			guard let firstValue = arguments.consumePositionalArgument() else {
+				if let defaultValue = argument.defaultValue {
+					return .success(defaultValue)
+				} else {
+					return .failure(missingArgumentError(argument.usage))
+				}
 			}
-		}
 
-		var values = [T]()
+			var values = [T]()
 
-		guard let value = T.from(string: firstValue) else {
-			return .failure(argument.invalidUsageError(firstValue))
-		}
-
-		values.append(value)
-
-		while let nextValue = arguments.consumePositionalArgument() {
-			guard let value = T.from(string: nextValue) else {
-				return .failure(argument.invalidUsageError(nextValue))
+			guard let value = T.from(string: firstValue) else {
+				return .failure(argument.invalidUsageError(firstValue))
 			}
 
 			values.append(value)
+
+			while let nextValue = arguments.consumePositionalArgument() {
+				guard let value = T.from(string: nextValue) else {
+					return .failure(argument.invalidUsageError(nextValue))
+				}
+
+				values.append(value)
+			}
+
+			return .success(values)
+
+		case .usage:
+			return .failure(informativeUsageError(argument))
 		}
-
-		return .success(values)
-
-	case .usage:
-		return .failure(informativeUsageError(argument))
 	}
 }
