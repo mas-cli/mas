fc8222a6a4cf676215f328c9132bb11c4b4c3965
Ben Chatelain <ben@octop.ad>
2018-01-28 19:35:27 -0700

    Add test target

---
+++ Tests/mas_tests.swift
@@ -0,0 +1,35 @@
+//
+//  mas_tests.swift
+//  mas-tests
+//
+//  Created by Ben Chatelain on 1/28/18.
+//  Copyright © 2018 Andrew Naylor. All rights reserved.
+//
+
+import XCTest
+
+class mas_tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
