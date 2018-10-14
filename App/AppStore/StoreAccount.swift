16307162ebc2739e5320188a7fe4ef895aee91fe
Unknown <ben@octop.ad>
2018-10-14 14:48:39 -0600

    ⌨️ Update types on StoreAccount protocol

--- App/AppStore/StoreAccount.swift
+++ App/AppStore/StoreAccount.swift
@@ -9,7 +9,7 @@
 protocol StoreAccount {
     static var primaryAccountIsPresentAndSignedIn: Bool { get }
     static var primaryAccount: StoreAccount? { get }
-    static func signIn(username: String?, password: String?, systemDialog: Bool) throws -> StoreAccount
+    static func signIn(username: String, password: String, systemDialog: Bool) throws -> StoreAccount
 
-    var identifier: String! { get set }
+    var identifier: String { get set }
 }
