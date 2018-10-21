ede7fc80193f385402faf167fb27a8d0b478aad9
Ben Chatelain <ben@octop.ad>
2018-10-20 22:34:49 -0600

    🍺🚰 Update tapp formula

--- Homebrew/mas-tap.rb
+++ Homebrew/mas-tap.rb
@@ -1,43 +1,45 @@
 class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
-  url "https://github.com/mas-cli/mas/archive/v1.4.2.tar.gz"
-  sha256 "f9a751ff84e6dcbaedd4b2ca95b3ca623c739fd3af0b6ca950c321f2ce840bfe"
+  url "https://github.com/mas-cli/mas.git",
+      :tag => "v1.4.3",
+      :revision => "11a0e3e14e5a83aaaba193dfb6d18aa49a82b881"
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
     root_url "https://dl.bintray.com/phatblat/mas-bottles"
     cellar :any_skip_relocation
-    sha256 "a99d0d7baecf45a00787365a2a078ae94068a0c63012e87f92282efb120e586e" => :el_capitan
-    sha256 "a99d0d7baecf45a00787365a2a078ae94068a0c63012e87f92282efb120e586e" => :high_sierra
-    sha256 "a99d0d7baecf45a00787365a2a078ae94068a0c63012e87f92282efb120e586e" => :mojave
-    sha256 "a99d0d7baecf45a00787365a2a078ae94068a0c63012e87f92282efb120e586e" => :sierra
-    sha256 "a99d0d7baecf45a00787365a2a078ae94068a0c63012e87f92282efb120e586e" => :yosemite
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :mojave
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :high_sierra
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :sierra
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :el_capitan
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :yosemite
+    sha256 "84a34f9e4458b7bed57e013cc85ecb1df5fa165d458fed3a9e8c4a9ac43baada" => :mavericks
   end
 
+  depends_on "carthage" => :build
   depends_on :xcode => ["10.0", :build]
 
-  resource "cocoapods" do
-    url "https://dl.bintray.com/phatblat/mas-bottles/master.tar.gz"
-    sha256 "fd8f1b06a2a0276c9005241b45cc19393b7c39cfc91d08da92a307ea2416e966"
-  end
-
   def install
-    # Pre-install a shallow copy of the CocoaPods master repo
-    (buildpath/".brew_home/.cocoapods/repos/master").install resource("cocoapods")
+    # Prevent warnings from causing build failures
+    # Prevent linker errors by telling all lib builds to use max size install names
+    xcconfig = buildpath/"Overrides.xcconfig"
+    xcconfig.write <<~EOS
+      GCC_TREAT_WARNINGS_AS_ERRORS = NO
+      OTHER_LDFLAGS = -headerpad_max_install_names
+    EOS
+    ENV["XCODE_XCCONFIG_FILE"] = xcconfig
 
-    # Install bundler, then use it to install gems used by project
-    ENV["GEM_HOME"] = buildpath/"gem_home"
-    system "gem", "install", "bundler"
-    ENV.prepend_path "PATH", buildpath/"gem_home/bin"
-    system "bundle", "install"
-    system "bundle", "exec", "pod", "install"
+    system "carthage", "bootstrap", "--platform", "macOS"
 
-    xcodebuild "-workspace", "mas-cli.xcworkspace",
-               "-scheme", "mas-cli Release",
-               "SYMROOT=#{buildpath.realpath}"
+    xcodebuild "install",
+                "-project", "mas-cli.xcodeproj",
+                "-scheme", "mas-cli Release",
+                "-configuration", "Release",
+                "OBJROOT=build",
+                "SYMROOT=build"
 
-    bin.install buildpath/"build/mas"
+    system "script/install", prefix
 
     bash_completion.install "contrib/completion/mas-completion.bash" => "mas"
   end
