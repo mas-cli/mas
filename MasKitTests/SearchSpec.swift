f57028c5e1125d1d0524a95de636842f560acd34
Ben Chatelain <ben@octop.ad>
2018-11-12 14:26:12 -0700

    ✅ Add search url string tests

--- MasKitTests/SearchSpec.swift
+++ MasKitTests/SearchSpec.swift
@@ -14,8 +14,24 @@ class SearchSpec: QuickSpec {
     override func spec() {
         describe("search") {
             describe("url string") {
-                it("is nil when app name cannot be url encoded") {
-                    let appName = "💩"
+                it("contains the app name") {
+                    let appName = "myapp"
+                    let search = SearchCommand()
+                    let urlString = search.searchURLString(appName)
+                    expect(urlString) ==
+                    "https://itunes.apple.com/search?entity=macSoftware&term=\(appName)&attribute=allTrackTerm"
+                }
+                it("contains the encoded app name") {
+                    let appName = "My App"
+                    let appNameEncoded = "My%20App"
+                    let search = SearchCommand()
+                    let urlString = search.searchURLString(appName)
+                    expect(urlString) ==
+                    "https://itunes.apple.com/search?entity=macSoftware&term=\(appNameEncoded)&attribute=allTrackTerm"
+                }
+                // FIXME: Find a character that causes addingPercentEncoding(withAllowedCharacters to return nil
+                xit("is nil when app name cannot be url encoded") {
+                    let appName = "`~!@#$%^&*()_+ 💩"
                     let search = SearchCommand()
                     let urlString = search.searchURLString(appName)
                     expect(urlString).to(beNil())
