07ef4b44cb512a525293e4eedb64a7b0488bf36d
Ben Chatelain <ben@octop.ad>
2019-01-01 20:44:44 -0800

    🤡 Add lookup JSON files

---
+++ MasKitTests/JSON/lookup/slack.js
@@ -0,0 +1,62 @@
+{
+    "resultCount": 1,
+    "results": [
+        {
+            "screenshotUrls": [
+                "https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/8d/07/74/8d0774c5-90aa-611c-3701-35f6158fb77e/source/800x500bb.jpg",
+                "https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/6d/86/a7/6d86a74d-5c45-1a61-7828-ff3251360271/source/800x500bb.jpg",
+                "https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/18/c1/38/18c138de-5bf2-586b-34de-c81e05568ea3/source/800x500bb.jpg",
+                "https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/4a/92/50/4a925081-61d0-ff32-eb2a-4b57db03aaab/source/800x500bb.jpg"
+            ],
+            "artworkUrl60": "https://is4-ssl.mzstatic.com/image/thumb/Purple128/v4/84/94/74/84947420-25dc-35e2-2761-9fa2b583c0a5/source/60x60bb.png",
+            "artworkUrl512": "https://is4-ssl.mzstatic.com/image/thumb/Purple128/v4/84/94/74/84947420-25dc-35e2-2761-9fa2b583c0a5/source/512x512bb.png",
+            "artworkUrl100": "https://is4-ssl.mzstatic.com/image/thumb/Purple128/v4/84/94/74/84947420-25dc-35e2-2761-9fa2b583c0a5/source/100x100bb.png",
+            "artistViewUrl": "https://itunes.apple.com/us/developer/slack-technologies-inc/id453420243?mt=12&uo=4",
+            "kind": "mac-software",
+            "averageUserRatingForCurrentVersion": 4,
+            "trackCensoredName": "Slack",
+            "languageCodesISO2A": [
+                "EN",
+                "FR",
+                "DE",
+                "JA",
+                "ES"
+            ],
+            "fileSizeBytes": "74398324",
+            "sellerUrl": "https://slack.com",
+            "contentAdvisoryRating": "4+",
+            "userRatingCountForCurrentVersion": 106,
+            "trackViewUrl": "https://itunes.apple.com/us/app/slack/id803453959?mt=12&uo=4",
+            "trackContentRating": "4+",
+            "releaseNotes": "All updates are important, of course. This one contains security updates, and as we know, they’re the most important kind of all.",
+            "currentVersionReleaseDate": "2018-10-02T23:28:05Z",
+            "isVppDeviceBasedLicensingEnabled": true,
+            "sellerName": "Slack Technologies, Inc.",
+            "primaryGenreId": 12001,
+            "primaryGenreName": "Business",
+            "genreIds": [
+                "12001",
+                "12014"
+            ],
+            "wrapperType": "software",
+            "version": "3.3.3",
+            "releaseDate": "2014-01-23T02:46:20Z",
+            "minimumOsVersion": "10.9",
+            "artistId": 453420243,
+            "artistName": "Slack Technologies, Inc.",
+            "genres": [
+                "Business",
+                "Productivity"
+            ],
+            "price": 0,
+            "currency": "USD",
+            "description": "Slack brings team communication and collaboration into one place so you can get more work done, whether you belong to a large enterprise or a small business. Check off your to-do list and move your projects forward by bringing the right people, conversations, tools, and information you need together. Slack is available on any device, so you can find and access your team and your work, whether you’re at your desk or on the go.\n\nUse Slack to: \n• Communicate with your team and organize your conversations by topics, projects, or anything else that matters to your work\n• Message or call any person or group within your team\n• Share and edit documents and collaborate with the right people all in Slack \n• Integrate into your workflow, the tools and services you already use including Google Drive, Salesforce, Dropbox, Asana, Twitter, Zendesk, and more\n• Easily search a central knowledge base that automatically indexes and archives your team’s past conversations and files\n• Customize your notifications so you stay focused on what matters\n\nScientifically proven (or at least rumored) to make your working life simpler, more pleasant, and more productive. We hope you’ll give Slack a try.\n\nStop by and learn more at: https://slack.com/",
+            "bundleId": "com.tinyspeck.slackmacgap",
+            "trackId": 803453959,
+            "trackName": "Slack",
+            "formattedPrice": "Free",
+            "userRatingCount": 1467,
+            "averageUserRating": 4
+        }
+    ]
+}
