7e72360e0640912d427a6f77a43698426da8b9cb
Ben Chatelain <ben@octop.ad>
2019-01-08 22:44:25 -0700

    🚧 Add OutputListenerSpec
    Doesn’t work to reuse OutputListener class in 2nd test

---
+++ MasKitTests/OutputListenerSpec.swift
@@ -0,0 +1,49 @@
+//
+//  OutputListenerSpec.swift
+//  MasKitTests
+//
+//  Created by Ben Chatelain on 1/8/19.
+//  Copyright © 2019 mas-cli. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class OutputListenerSpec: QuickSpec {
+    override func spec() {
+        // FIXME: Re-using OutputListener in a 2nd test fails with no content
+        xdescribe("output listener") {
+            it("can intercept a single line written stdout") {
+                let output = OutputListener()
+                output.openConsolePipe()
+
+                let expectedOutput = "hi there"
+
+                print("hi there", terminator: "")
+
+                // output is async so need to wait for contents to be updated
+                expect(output.contents).toEventuallyNot(beEmpty())
+                expect(output.contents) == expectedOutput
+
+                output.closeConsolePipe()
+            }
+            it("can intercept multiple lines written stdout") {
+                let output = OutputListener()
+                output.openConsolePipe()
+
+                let expectedOutput = """
+                    hi there
+
+                    """
+
+                print("hi there")
+
+                // output is async so need to wait for contents to be updated
+                expect(output.contents).toEventuallyNot(beEmpty())
+                expect(output.contents) == expectedOutput
+
+                output.closeConsolePipe()
+            }
+        }
+    }
+}
