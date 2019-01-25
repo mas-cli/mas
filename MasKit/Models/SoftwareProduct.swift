7f71f170e9a6f904eb7a37e7413d2b0075d2f70c
Ben Chatelain <ben@octop.ad>
2019-01-25 11:58:16 -0700

    🚧 Expose more properties in SoftwareProduct

--- MasKit/Models/SoftwareProduct.swift
+++ MasKit/Models/SoftwareProduct.swift
@@ -8,11 +8,22 @@
 
 /// Protocol describing the members of CKSoftwareProduct used throughout MasKit.
 public protocol SoftwareProduct {
+    var accountIdentifier: String { get }
+    var accountOpaqueDSID: String { get }
     var appName: String { get }
     var bundleIdentifier: String { get set }
     var bundlePath: String { get set }
     var bundleVersion: String { get set }
+    var description: String { get }
+    var expectedBundleVersion: String? { get set }
+    var expectedStoreVersion: NSNumber? { get set }
+//    var mdItemRef: NSValue { get set }
+    var installed: Bool { get set }
+    var isLegacyApp: Bool { get set }
+    var isMachineLicensed: Bool { get set }
     var itemIdentifier: NSNumber { get set }
+    var purchaseDate: Date { get set }
+    var storeFrontIdentifier: NSNumber { get set }
 }
 
 // MARK: - Equatable
--- MasKitTests/Models/SoftwareProductMock.swift
+++ MasKitTests/Models/SoftwareProductMock.swift
@@ -9,9 +9,60 @@
 @testable import MasKit
 
 struct SoftwareProductMock: SoftwareProduct {
+    var accountIdentifier: String
+    var accountOpaqueDSID: String
+    var bundleIdentifier: String
+    var description: String
+    var expectedBundleVersion: String?
+    var expectedStoreVersion: NSNumber?
+//    var mdItemRef: NSValue
+    var installed: Bool
+    var isLegacyApp: Bool
+    var isMachineLicensed: Bool
+    var purchaseDate: Date
+    var storeFrontIdentifier: NSNumber
+
     var appName: String
     var bundleIdentifier: String
     var bundlePath: String
     var bundleVersion: String
     var itemIdentifier: NSNumber
+
+    init(
+        accountIdentifier: String = "",
+        accountOpaqueDSID: String = "",
+        bundleIdentifier: String = "",
+        description: String = "",
+        expectedBundleVersion: String? = nil,
+        expectedStoreVersion: NSNumber? = nil,
+//        mdItemRef: NSValue,
+        installed: Bool = false,
+        isLegacyApp: Bool = false,
+        isMachineLicensed: Bool = false,
+        purchaseDate: Date = Date(),
+        storeFrontIdentifier: NSNumber = 0,
+
+        appName: String = "",
+        bundlePath: String = "",
+        bundleVersion: String = "",
+        itemIdentifier: NSNumber = 0
+    ) {
+        self.accountIdentifier = accountIdentifier
+        self.accountOpaqueDSID = accountOpaqueDSID
+        self.bundleIdentifier = bundleIdentifier
+        self.description = description
+        self.expectedBundleVersion = expectedBundleVersion
+        self.expectedStoreVersion = expectedStoreVersion
+//        self.mdItemRef = mdItemRef
+        self.installed = installed
+        self.isLegacyApp = isLegacyApp
+        self.isMachineLicensed = isMachineLicensed
+        self.purchaseDate = purchaseDate
+        self.storeFrontIdentifier = storeFrontIdentifier
+
+        self.appName = appName
+        self.bundlePath = bundlePath
+        self.bundleVersion = bundleVersion
+        self.itemIdentifier = itemIdentifier
+    }
 }
