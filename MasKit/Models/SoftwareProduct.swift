9f919145395079f5ca5e4a31ece10bbbf992a4cc
Ben Chatelain <ben@octop.ad>
2019-03-30 16:23:35 -0600

    💡 Document SoftwareProduct properties

--- MasKit/Models/SoftwareProduct.swift
+++ MasKit/Models/SoftwareProduct.swift
@@ -10,28 +10,46 @@ import Foundation
 
 /// Protocol describing the members of CKSoftwareProduct used throughout MasKit.
 public protocol SoftwareProduct {
+    /// Not used
     var accountIdentifier: String { get }
+    /// Not used
     var accountOpaqueDSID: String { get }
 
-    /// Display name of app.
-    /// Empty string on 10.14.4 (see #226).
+    /// Display name of app. Empty string on 10.14.4 (see #226).
     var appName: String { get }
+
+    /// System identifier for apps on macOS.
     var bundleIdentifier: String { get set }
+
+    /// Path to .app bundle.
     var bundlePath: String { get set }
+
+    /// Version of app
     var bundleVersion: String { get set }
+
+    /// Summary of type for debugging.
     var description: String { get }
+
+    /// Not used
     var expectedBundleVersion: String? { get set }
+    /// Not used
     var expectedStoreVersion: NSNumber? { get set }
+    /// Not used
     var mdItemRef: NSValue? { get set }
+    /// Not used
     var installed: Bool { get set }
+    /// Not used
     var isLegacyApp: Bool { get set }
+    /// Not used
     var isMachineLicensed: Bool { get set }
 
-    /// zero (or nil) for macOS installers
+    /// App Store identifier for apps. zero (or nil?) for macOS installers.
     var itemIdentifier: NSNumber { get set }
 
     /// Not populated for macOS installers.
     var purchaseDate: Date? { get set }
+
+    /// Not used
     var storeFrontIdentifier: NSNumber? { get set }
 }
 
