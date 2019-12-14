f28355374e55ef39c0a1b22cdd295747a94b4077
Ben Chatelain <ben@octop.ad>
2019-12-13 17:47:44 -0700

    🔥 Remove obsolete Result sources

--- Carthage/Checkouts/Nimble/Carthage/Checkouts/CwlCatchException/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-	name: "CwlCatchException",
-	targets: [
-		Target(name: "CwlCatchException", dependencies: ["CwlCatchExceptionSupport"]),
-		Target(name: "CwlCatchExceptionSupport")
-	]
-)
