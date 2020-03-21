fbfe5385ee69585f90934c6c5810c35b1213de2b
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2020-03-21 12:31:46 +0100

    Add tests for purchase command

---
+++ MasKitTests/Commands/PurchaseCommandSpec.swift
@@ -0,0 +1,24 @@
+//
+//  PurchaseCommandSpec.swift
+//  MasKitTests
+//
+//  Created by Maximilian Blochberger on 2020-03-21.
+//  Copyright © 2020 mas-cli. All rights reserved.
+//
+
+@testable import MasKit
+import Nimble
+import Quick
+
+class PurchaseCommandSpec: QuickSpec {
+    override func spec() {
+        describe("purchase command") {
+            it("purchases apps") {
+                let cmd = PurchaseCommand()
+                let result = cmd.run(PurchaseCommand.Options(appIds: []))
+                print(result)
+//                expect(result).to(beSuccess())
+            }
+        }
+    }
+}
