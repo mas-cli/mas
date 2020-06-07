c2545346555f4778524976420240d4244ecaa8fa
Ben Chatelain <ben@octop.ad>
2020-06-07 14:01:34 -0600

    🔈 Format list output using AppListFormatter

---
+++ MasKit/Formatters/AppListFormatter.swift
@@ -0,0 +1,43 @@
+//
+//  AppListFormatter.swift
+//  MasKit
+//
+//  Created by Ben Chatelain on 6/7/20.
+//  Copyright © 2019 mas-cli. All rights reserved.
+//
+
+import Foundation
+
+/// Formats text output for the search command.
+struct AppListFormatter {
+    /// Formats text output with list results.
+    ///
+    /// - Parameter products: List of sortware products app data.
+    /// - Returns: Multiliune text outoutp.
+    static func format(products: [SoftwareProduct]) -> String {
+
+        // TODO: Catch empty appNames
+        //        var appName = product.appName
+        //        if appName == "" {
+        //            appName = product.bundleIdentifier
+        //        }
+
+        let minWidth = 50
+        // find longest appName for formatting, default 50
+        let maxLength = products.map { $0.appName }
+            .max(by: { $1.count > $0.count })?.count
+            ?? minWidth
+
+        var output: String = ""
+
+        for product in products {
+            let appId = product.itemIdentifier.intValue
+            let appName = product.appName.padding(toLength: maxLength, withPad: " ", startingAt: 0)
+            let version = product.bundleVersion
+
+            output += String(format: "%12d  %@  (%@)\n", appId, appName, version)
+        }
+
+        return output.trimmingCharacters(in: .newlines)
+    }
+}
