4bbf9753300fdbce76545707782ad25b3b08a018
Ben Chatelain <ben@octop.ad>
2020-08-23 14:01:18 -0600

    🐛 Fix alignment of 2nd column of list command output

--- MasKit/Formatters/AppListFormatter.swift
+++ MasKit/Formatters/AppListFormatter.swift
@@ -10,21 +10,25 @@ import Foundation
 
 /// Formats text output for the search command.
 struct AppListFormatter {
+
+    static let idColumnMinWidth = 10
+    static let nameColumnMinWidth = 50
+
     /// Formats text output with list results.
     ///
     /// - Parameter products: List of sortware products app data.
     /// - Returns: Multiliune text outoutp.
     static func format(products: [SoftwareProduct]) -> String {
         // find longest appName for formatting, default 50
-        let minWidth = 50
         let maxLength = products.map { $0.appNameOrBbundleIdentifier }
             .max(by: { $1.count > $0.count })?.count
-            ?? minWidth
+            ?? nameColumnMinWidth
 
         var output: String = ""
 
         for product in products {
-            let appId = product.itemIdentifier
+            let appId = product.itemIdentifier.stringValue
+                .padding(toLength: idColumnMinWidth, withPad: " ", startingAt: 0)
             let appName = product.appNameOrBbundleIdentifier.padding(toLength: maxLength, withPad: " ", startingAt: 0)
             let version = product.bundleVersion
 
