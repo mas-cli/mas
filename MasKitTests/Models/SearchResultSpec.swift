27839a57abda2b7250673c9aaa0b6e4f058f1409
Ben Chatelain <ben@octop.ad>
2020-09-06 14:21:28 -0600

    ✅ Add SearchResultListSpec

---
+++ MasKitTests/Models/SearchResultSpec.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultSpec.swift
+//  MasKitTests
+//
+//  Created by Ben Chatelain on 9/2/20.
+//  Copyright © 2020 mas-cli. All rights reserved.
+//
+
+@testable import MasKit
+import Quick
+import Nimble
+
+class SearchResultSpec: QuickSpec {
+    override func spec() {
+        describe("search result") {
+
+        }
+    }
+}
