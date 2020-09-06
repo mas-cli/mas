1286b70e7e602709a84f4013cff5ca2bafd33dc8
Ben Chatelain <ben@octop.ad>
2020-09-06 14:15:02 -0600

    ♻️ Revise API for local JSON fixtures
    Bundle.url(for:)

--- MasKitTests/Controllers/MasStoreSearchSpec.swift
+++ MasKitTests/Controllers/MasStoreSearchSpec.swift
@@ -12,50 +12,53 @@ import Quick
 
 class MasStoreSearchSpec: QuickSpec {
     override func spec() {
-        describe("store search") {
-            it("can find slack") {
-                let networkSession = NetworkSessionMockFromFile(responseFile: "search/slack.json")
-                let storeSearch = MasStoreSearch(networkManager: NetworkManager(session: networkSession))
+        describe("store") {
+            context("when searched") {
+                it("can find slack") {
+                    let networkSession = NetworkSessionMockFromFile(responseFile: "search/slack.json")
+                    let storeSearch = MasStoreSearch(networkManager: NetworkManager(session: networkSession))
 
-                var searchList: SearchResultList
-                do {
-                    searchList = try storeSearch.search(for: "slack")
-                    expect(searchList.resultCount) == 6
-                    expect(searchList.results.count) == 6
-                } catch {
-                    let maserror = error as! MASError
-                    if case let .jsonParsing(nserror) = maserror {
-                        fail("\(maserror) \(nserror!)")
+                    var searchList: SearchResultList
+                    do {
+                        searchList = try storeSearch.search(for: "slack")
+                        expect(searchList.resultCount) == 39
+                        expect(searchList.results.count) == 39
+                    } catch {
+                        let maserror = error as! MASError
+                        if case let .jsonParsing(nserror) = maserror {
+                            fail("\(maserror) \(nserror!)")
+                        }
                     }
                 }
             }
-        }
-        describe("store lookup") {
-            it("can find slack") {
-                let appId = 803_453_959
-                let networkSession = NetworkSessionMockFromFile(responseFile: "lookup/slack.json")
-                let storeSearch = MasStoreSearch(networkManager: NetworkManager(session: networkSession))
 
-                var lookup: SearchResult?
-                do {
-                    lookup = try storeSearch.lookup(app: appId)
-                } catch {
-                    let maserror = error as! MASError
-                    if case let .jsonParsing(nserror) = maserror {
-                        fail("\(maserror) \(nserror!)")
+            context("when lookup used") {
+                it("can find slack") {
+                    let appId = 803_453_959
+                    let networkSession = NetworkSessionMockFromFile(responseFile: "lookup/slack.json")
+                    let storeSearch = MasStoreSearch(networkManager: NetworkManager(session: networkSession))
+
+                    var lookup: SearchResult?
+                    do {
+                        lookup = try storeSearch.lookup(app: appId)
+                    } catch {
+                        let maserror = error as! MASError
+                        if case let .jsonParsing(nserror) = maserror {
+                            fail("\(maserror) \(nserror!)")
+                        }
                     }
+
+                    guard let result = lookup else { fatalError("lookup result was nil") }
+
+                    expect(result.trackId) == appId
+                    expect(result.bundleId) == "com.tinyspeck.slackmacgap"
+                    expect(result.price) == 0
+                    expect(result.sellerName) == "Slack Technologies, Inc."
+                    expect(result.sellerUrl) == "https://slack.com"
+                    expect(result.trackName) == "Slack"
+                    expect(result.trackViewUrl) == "https://itunes.apple.com/us/app/slack/id803453959?mt=12&uo=4"
+                    expect(result.version) == "3.3.3"
                 }
-
-                guard let result = lookup else { fatalError("lookup result was nil") }
-
-                expect(result.trackId) == appId
-                expect(result.bundleId) == "com.tinyspeck.slackmacgap"
-                expect(result.price) == 0
-                expect(result.sellerName) == "Slack Technologies, Inc."
-                expect(result.sellerUrl) == "https://slack.com"
-                expect(result.trackName) == "Slack"
-                expect(result.trackViewUrl) == "https://itunes.apple.com/us/app/slack/id803453959?mt=12&uo=4"
-                expect(result.version) == "3.3.3"
             }
         }
     }
