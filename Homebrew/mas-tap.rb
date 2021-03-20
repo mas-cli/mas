7e3c03b43d3daf2e44664f343f77e1edaf5c58ab
Ben Chatelain <ben@octop.ad>
2021-03-20 08:49:44 -0700

    🍺 Update version, add macos dependency to formula

--- Homebrew/mas-tap.rb
+++ Homebrew/mas-tap.rb
@@ -2,8 +2,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.0",
-      revision: "9eaf57a5de836ce5e5435a8df14da4aa1b7d7444"
+      tag:      "v1.8.1",
+      revision: "23a36b4555f5625fe29915b31b8b101064452dca"
   license "MIT"
   head "https://github.com/mas-cli/mas.git"
 
@@ -19,6 +19,7 @@ class Mas < Formula
   end
 
   depends_on "carthage" => :build
+  depends_on :macos
   if Hardware::CPU.arm?
     depends_on xcode: ["12.2", :build]
   else
