e7b2ca88decb480fc1b0ab302d1b54a55c94ad8d
Chris Araman <chris@hiddenplace.dev>
2021-08-29 13:05:09 -0700

    ⬆️ Nimble (9.2.1)

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.18.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.1"),
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.3"),
         .package(url: "https://github.com/mxcl/Version.git", from: "2.0.1"),
