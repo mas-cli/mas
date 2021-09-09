a61e9a1b8047dd9ffa37e7d62c1751a26b0e1be9
Ben Chatelain <ben@octop.ad>
2021-09-08 19:09:20 -0600

    🍺 Update formula version and custom tap bottle hashes

--- Homebrew/mas-tap.rb
+++ Homebrew/mas-tap.rb
@@ -2,20 +2,20 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.2",
-      revision: "2f2a43b425498f3cee50974116b8c9d27adbb7cb"
+      tag:      "v1.8.3",
+      revision: "aeeb1c508e98d657769ef4e368a113be7822d92e"
   license "MIT"
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
-    root_url "https://github.com/mas-cli/mas/releases/download/v1.8.2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, catalina:      "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, mojave:        "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, sierra:        "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    root_url "https://github.com/mas-cli/mas/releases/download/v1.8.3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, catalina:      "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, mojave:        "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, sierra:        "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "3be8fedcbe67571100d0ec45f9ed4d68a22db93e406806a4365568138dd33bb7"
   end
 
   depends_on :macos
