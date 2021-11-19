be324bb060e3d718e76ae8d5e4dfaaf5258fdbc1
Chris Araman <chris@hiddenplace.dev>
2021-11-19 15:20:14 -0800

    ⬆️ PromiseKit (6.16.2)

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.18.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.1"),
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.1"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.2"),
         .package(url: "https://github.com/mxcl/Version.git", from: "2.0.1"),
     ],
     targets: [
