8033470c83c95e4cb9fd2feba2603a3f896010ce
Ben Chatelain <ben@octop.ad>
2022-09-21 19:39:11 -0600

    ⬆️ Quick (5.0.1), Nimble (10.0.0)

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.18.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.1"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "5.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.2"),
         .package(url: "https://github.com/mxcl/Version.git", from: "2.0.1"),
     ],
