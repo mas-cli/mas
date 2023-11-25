c2fcf7e5aaa4e165bcad0f19807c705496ce3210
Ben Chatelain <ben@octop.ad>
2023-11-25 13:00:37 -0700

    💡 Document deprecated AccountCommandSpec and SignInCommandSpec

--- Tests/MasKitTests/Commands/AccountCommandSpec.swift
+++ Tests/MasKitTests/Commands/AccountCommandSpec.swift
@@ -11,12 +11,14 @@ import Quick
 
 @testable import MasKit
 
+// Deprecated test
 public class AccountCommandSpec: QuickSpec {
     override public func spec() {
         beforeSuite {
             MasKit.initialize()
         }
-        describe("Account command") {
+        // account command disabled since macOS 12 Monterey https://github.com/mas-cli/mas#%EF%B8%8F-known-issues
+        xdescribe("Account command") {
             xit("displays active account") {
                 let cmd = AccountCommand()
                 let result = cmd.run(AccountCommand.Options())
--- Tests/MasKitTests/Commands/SignInCommandSpec.swift
+++ Tests/MasKitTests/Commands/SignInCommandSpec.swift
@@ -11,12 +11,14 @@ import Quick
 
 @testable import MasKit
 
+// Deprecated test
 public class SignInCommandSpec: QuickSpec {
     override public func spec() {
         beforeSuite {
             MasKit.initialize()
         }
-        describe("signin command") {
+        // account command disabled since macOS 10.13 High Sierra https://github.com/mas-cli/mas#%EF%B8%8F-known-issues
+        xdescribe("signin command") {
             xit("signs in") {
                 let cmd = SignInCommand()
                 let result = cmd.run(SignInCommand.Options(username: "", password: "", dialog: false))
