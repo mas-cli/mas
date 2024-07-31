485c5839970603ef887b944866c733dcfbe141e7
Anka <runner@Mac-1722393303430.local>
2024-07-31 02:48:56 +0000

    🔖 Version v1.8.7-beta.21

--- Homebrew/mas-tap.rb
+++ Homebrew/mas-tap.rb
@@ -2,8 +2,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.7-beta.1",
-      revision: "f8be3e9aaa6c78490277976f17041f2577f5dc21"
+      tag:      "v1.8.7-beta.21",
+      revision: "releases/release-1.8.7"
   license "MIT"
   head "https://github.com/mas-cli/mas.git", branch: "main"
 
