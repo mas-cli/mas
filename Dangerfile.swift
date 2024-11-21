2bffa05d0d1664ff7b501a23c6e5710b0b2a63b5
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2024-11-20 22:58:47 -0500

    Cleanup Swift files.
    Partial #638
    
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,19 +1,24 @@
 import Danger
+
 let danger = Danger()
 
-// Thanks other people!
 if let github = danger.github {
+    // Thank non-member submitters
     let submitter = github.pullRequest.user.login
-    if submitter != "phatblat" && submitter != "chris-araman" {
+    switch submitter {
+    case "chris-araman",
+        "phatblat":
+        break
+    default:
         danger.message(":tada: Thanks for your contribution, \(submitter)!")
     }
 
-    // Mainly to encourage writing up some reasoning about the PR, rather than just leaving a title
+    // Encourage writing up some reasoning about the PR
     if github.pullRequest.body?.count ?? 0 < 5 {
         danger.fail(":memo: Please provide a summary in the Pull Request description.")
     }
 
-    // Make it more obvious that a PR is a work in progress and shouldn't be merged yet
+    // Warn that PR marked [WIP] should be a Draft
     if github.pullRequest.title.contains("[WIP]") {
         danger.warn(":construction: Title includes `[WIP]`. Please convert the pull request to a Draft.")
     }
