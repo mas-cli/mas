ed1d272109f592a575d7c06956bbf964d9cf57db
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-01-02 02:46:05 -0500

    Improve `Reset`. Remove `--debug` flag.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Commands/Reset.swift
+++ Sources/mas/Commands/Reset.swift
@@ -9,74 +9,67 @@ internal import ArgumentParser
 private import CommerceKit
 
 extension MAS {
-	/// Kills several macOS processes as a means to reset the app store.
+	/// Terminates several macOS processes & deletes files to reset the Mac App
+	/// Store.
 	struct Reset: ParsableCommand {
 		static let configuration = CommandConfiguration(
-			abstract: "Reset Mac App Store running processes"
+			abstract: "Reset Mac App Store processes"
 		)
-
-		@Flag(help: "Output debug information")
-		var debug = false
 
 		/// Runs the command.
 		func run() throws {
 			try mas.run { run(printer: $0) }
 		}
 
+		/// The "Reset Application" command in the Mac App Store debug menu performs
+		/// the following steps:
+		///
+		/// - `killall Dock`
+		/// - `killall storeagent` (`storeagent` no longer exists)
+		/// - deletes the `com.apple.appstore` download folder
+		/// - clears cookies (appears to be a no-op)
+		///
+		/// As `storeagent` no longer exists, terminates all processes known to be
+		/// associated with the Mac App Store.
 		func run(printer: Printer) {
-			// The "Reset Application" command in the Mac App Store debug menu performs
-			// the following steps
-			//
-			// - killall Dock
-			// - killall storeagent (storeagent no longer exists)
-			// - rm com.apple.appstore download directory
-			// - clear cookies (appears to be a no-op)
-			//
-			// As storeagent no longer exists we will implement a slight variant & kill all
-			// App Store-associated processes
-			// - storeaccountd
-			// - storeassetd
-			// - storedownloadd
-			// - storeinstalld
-			// - storelegacy
-
-			// Kill processes
-			let killProcs = [
+			let killall = Process()
+			killall.launchPath = "/usr/bin/killall"
+			killall.arguments = [
+				"ContextStoreAgent",
 				"Dock",
+				"SetStoreUpdateService",
+				"appstoreagent",
+				"appstorecomponentsd",
 				"storeaccountd",
 				"storeassetd",
 				"storedownloadd",
 				"storeinstalld",
+				"storekitagent",
 				"storelegacy",
+				"storeuid",
 			]
-
-			let kill = Process()
-			let stdout = Pipe()
 			let stderr = Pipe()
+			killall.standardError = stderr
+			killall.standardOutput = Pipe()
 
-			kill.launchPath = "/usr/bin/killall"
-			kill.arguments = killProcs
-			kill.standardOutput = stdout
-			kill.standardError = stderr
-
-			kill.launch()
-			kill.waitUntilExit()
-
-			if kill.terminationStatus != 0 {
+			killall.launch()
+			killall.waitUntilExit()
+			if killall.terminationStatus != 0 {
 				let output = stderr.fileHandleForReading.readDataToEndOfFile()
 				printer.error(
-					"killall failed:",
+					"killall failed with exit status ",
+					killall.terminationStatus,
+					":\n",
 					String(data: output, encoding: .utf8) ?? "Error info not available",
-					separator: "\n"
+					separator: ""
 				)
 			}
 
-			// Wipe Download Directory
-			let directory = CKDownloadDirectory(nil)
+			let folder = CKDownloadDirectory(nil)
 			do {
-				try FileManager.default.removeItem(atPath: directory)
+				try FileManager.default.removeItem(atPath: folder)
 			} catch {
-				printer.error("Failed to delete download directory", directory, error: error)
+				printer.error("Failed to delete download folder", folder, error: error)
 			}
 		}
 	}
