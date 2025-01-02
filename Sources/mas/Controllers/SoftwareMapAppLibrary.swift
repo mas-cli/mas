e68ef510e0f03864756e5959b91edf68af1ed05f
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-01-02 08:46:12 -0500

    Revert `SoftwareMapAppLibrary.swift` to a class.
    Partial #696
    
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Controllers/SoftwareMapAppLibrary.swift
+++ Sources/mas/Controllers/SoftwareMapAppLibrary.swift
@@ -10,17 +10,24 @@ import CommerceKit
 import ScriptingBridge
 
 /// Utility for managing installed apps.
-struct SoftwareMapAppLibrary: AppLibrary {
+class SoftwareMapAppLibrary: AppLibrary {
+    /// CommerceKit's singleton manager of installed software.
+    private let softwareMap: SoftwareMap
+
     /// Array of installed software products.
-    let installedApps: [SoftwareProduct]
+    lazy var installedApps = softwareMap.allSoftwareProducts()
+        .filter { product in
+            product.bundlePath.starts(with: "/Applications/")
+        }
 
     /// Internal initializer for providing a mock software map.
     /// - Parameter softwareMap: SoftwareMap to use
     init(softwareMap: SoftwareMap = CKSoftwareMap.shared()) {
-        installedApps = softwareMap.allSoftwareProducts()
-            .filter { product in
-                product.bundlePath.starts(with: "/Applications/")
-            }
+        self.softwareMap = softwareMap
+    }
+
+    deinit {
+        // do nothing
     }
 
     /// Uninstalls all apps located at any of the elements of `appPaths`.
