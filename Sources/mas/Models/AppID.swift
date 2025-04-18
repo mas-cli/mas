485bb85a02fd35e7636936f95f6904508a8aac3f
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-04-18 03:47:46 -0400

    Support apps under `/Volumes/*/Applications`.
    Remove `CKSoftwareProduct` & `CKSoftwareMap` extensions.
    
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Models/AppID.swift
+++ Sources/mas/Models/AppID.swift
@@ -6,19 +6,10 @@
 //  Copyright © 2024 mas-cli. All rights reserved.
 //
 
-import Foundation
-
 typealias AppID = UInt64
 
 extension AppID {
     var unknownMessage: String {
         "Unknown app ID \(self)"
-    }
-}
-
-// swiftlint:disable:next legacy_objc_type
-extension NSNumber {
-    var appIDValue: AppID {
-        uint64Value
     }
 }
