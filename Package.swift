6d135b31346f991ec4889bb9f7d998cd95aa0255
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-05-22 01:19:24 -0400

    Use main branch of Swift Argument Parser.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/Quick/Nimble.git", from: "13.7.1"),
 		.package(url: "https://github.com/Quick/Quick.git", exact: "7.5.0"),
-		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
+		.package(url: "https://github.com/apple/swift-argument-parser.git", branch: "main"),
 		.package(url: "https://github.com/apple/swift-atomics.git", branch: "main"),
 		.package(url: "https://github.com/funky-monkey/IsoCountryCodes.git", from: "1.0.2"),
 		.package(url: "https://github.com/mxcl/Version.git", from: "2.1.0"),
