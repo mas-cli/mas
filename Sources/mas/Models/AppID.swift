31ce24c41d43541dc8fc3cba7900513f3d17c9e0
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-11-03 06:10:30 -0500

    Remove unnecessary `Hashable` conformance from `AppID`.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Models/AppID.swift
+++ Sources/mas/Models/AppID.swift
@@ -5,7 +5,7 @@
 // Copyright © 2024 mas-cli. All rights reserved.
 //
 
-enum AppID: CustomStringConvertible, Hashable {
+enum AppID: CustomStringConvertible {
 	case adamID(ADAMID)
 	case bundleID(String)
 
