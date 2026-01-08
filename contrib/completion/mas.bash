f975f94f65a5ebd3b09802edcd55dfd5ac0b718f
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-01-07 23:26:20 -0500

    Fix bash completions & rename as `mas.bash`.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- contrib/completion/mas.bash
+++ contrib/completion/mas.bash
@@ -2,28 +2,33 @@
 
 _mas() {
 	local cur prev words cword
-	if declare -F _init_completions >/dev/null 2>&1; then
+	local shell_opts="$(shopt -p extglob)"
+	shopt -s extglob
+
+	if declare -F _init_completion >/dev/null 2>&1; then
 		_init_completion
 	else
 		COMPREPLY=()
-		_get_comp_words_by_ref cur prev words cword
+		_comp_get_words cur prev words cword
 	fi
 	if [[ "${cword}" -eq 1 ]]; then
 		local -r ifs_old="${IFS}"
 		IFS=$'\n'
 		local -a mas_help=($(mas help))
-		mas_help=("${mas_help[@]:5:${#mas_help[@]}-6}")
+		mas_help=("${mas_help[@]:6:${#mas_help[@]}-7}")
 		mas_help=("${mas_help[@]#  }")
 		local -a commands=(help)
 		for line in "${mas_help[@]}"; do
 			if [[ ! "${line}" =~ ^\  ]]; then
-				commands+=("${line%% *}")
+				commands+=("${line%%?(,) *}")
 			fi
 		done
 		COMPREPLY=($(compgen -W "${commands[*]}" -- "${cur}"))
 		IFS="${ifs_old}"
+		eval "${shell_opts}"
 		return 0
 	fi
+	eval "${shell_opts}"
 }
 
 complete -F _mas mas
