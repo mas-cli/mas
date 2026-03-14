504b3505a423512fce810163f25df5836f9b5c9d
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-03-14 01:00:15 -0400

    Inject `dataFrom` dependency.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Utilities/Dependencies.swift
+++ Sources/mas/Utilities/Dependencies.swift
@@ -5,13 +5,21 @@
 // Copyright © 2026 mas-cli. All rights reserved.
 //
 
+internal import Foundation
+
 struct Dependencies {
 	@TaskLocal
 	static var current = Self()
 
+	let dataFrom: @Sendable (URL) async throws -> (Data, URLResponse)
 	let lookupAppFromAppID: @Sendable (AppID) async throws -> CatalogApp
 
-	init(lookupAppFromAppID: @escaping @Sendable (AppID) async throws -> CatalogApp = lookup(appID:)) {
+	init(
+		dataFrom: @escaping @Sendable (URL) async throws -> (Data, URLResponse)
+		= URLSession(configuration: .ephemeral).data(from:), // swiftformat:disable:this indent
+		lookupAppFromAppID: @escaping @Sendable (AppID) async throws -> CatalogApp = lookup(appID:),
+	) {
+		self.dataFrom = dataFrom
 		self.lookupAppFromAppID = lookupAppFromAppID
 	}
 }
