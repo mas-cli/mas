709563d0ba82d8d52396cbacb39ec9d943d406a8
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-03-14 01:00:16 -0400

    Output JSON for `outdated`.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Commands/Outdated.swift
+++ Sources/mas/Commands/Outdated.swift
@@ -7,6 +7,8 @@
 
 internal import ArgumentParser
 private import Foundation
+private import JSONAST
+private import JSONParsing
 
 extension MAS {
 	/// Outputs a list of installed apps which have updates available to be
@@ -16,6 +18,8 @@ extension MAS {
 			abstract: "List pending app updates from the App Store",
 		)
 
+		@OptionGroup
+		private var outputFormatOptionGroup: OutputFormatOptionGroup
 		@OptionGroup
 		private var outdatedAppOptionGroup: OutdatedAppOptionGroup
 		@OptionGroup
@@ -39,24 +43,24 @@ extension MAS {
 		}
 
 		private func run(outdatedApps: [OutdatedApp]) {
-			guard
-				let maxADAMIDLength = outdatedApps.map({ String(describing: $0.installedApp.adamID).count }).max(),
-				let maxNameLength = outdatedApps.map(\.installedApp.name.count).max(),
-				let maxVersionLength = outdatedApps.map(\.installedApp.version.count).max()
-			else {
+			guard !outdatedApps.isEmpty else {
 				return
 			}
 
-			let format = "%\(maxADAMIDLength)lu  %@  (%@ -> %@)"
-			printer.info(
-				outdatedApps.map { installedApp, newVersion in
-					String(
-						format: format,
-						installedApp.adamID,
-						installedApp.name.padding(toLength: maxNameLength, withPad: " ", startingAt: 0),
-						installedApp.version.padding(toLength: maxVersionLength, withPad: " ", startingAt: 0),
-						newVersion,
-					)
+			outputFormatOptionGroup.info(
+				outdatedApps.compactMap { installedApp, newVersion in
+					do {
+						let newVersionKey = "newVersion"
+						var json = try JSON.Object(parsing: String(describing: installedApp))
+						json.fields.insert(
+							(JSON.Key(rawValue: newVersionKey), JSON.Node.string(JSON.Literal(newVersion))),
+							at: json.fields.enumerated().first { newVersionKey < $1.key.rawValue }?.offset ?? json.fields.count,
+						) // swiftlint:disable:previous unused_enumerated
+						return String(json)
+					} catch {
+						printer.error("Failed to parse outdated app JSON", installedApp, error: error, separator: "\n")
+						return nil
+					}
 				}
 				.joined(separator: "\n"),
 			)
