9f5af79c2115c84322c554053dfea6768f4d0431
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-03-28 07:02:39 -0400

    Allow `Collection.concurrentMap(…)` to work for `T is Optional`.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Utilities/Swift/Collection.swift
+++ Sources/mas/Utilities/Swift/Collection.swift
@@ -30,14 +30,14 @@ extension Collection where Element: Sendable {
 		maxConcurrentTaskCount: Int = defaultMaxConcurrentTaskCount,
 		_ transform: @escaping @Sendable (Element) async -> T?,
 	) async -> [T] {
-		await concurrentTransform(maxConcurrentTaskCount: maxConcurrentTaskCount, transform)
+		await concurrentCompactTransform(maxConcurrentTaskCount: maxConcurrentTaskCount, transform)
 	}
 
 	func concurrentCompactMap<T: Sendable>( // swiftlint:disable:this unused_declaration
 		maxConcurrentTaskCount: Int = defaultMaxConcurrentTaskCount,
 		_ transform: @escaping @Sendable (Element) async throws -> T?,
 	) async rethrows -> [T] { // periphery:ignore
-		try await concurrentTransform(maxConcurrentTaskCount: maxConcurrentTaskCount, transform)
+		try await concurrentCompactTransform(maxConcurrentTaskCount: maxConcurrentTaskCount, transform)
 	}
 
 	func concurrentCompactMap<T: Sendable, E: Error>(
@@ -56,6 +56,30 @@ extension Collection where Element: Sendable {
 	}
 
 	private func concurrentTransform<T: Sendable>(
+		maxConcurrentTaskCount: Int,
+		_ transform: @escaping @Sendable (Element) async throws -> T,
+	) async rethrows -> [T] {
+		try await withThrowingTaskGroup(of: (index: Int, result: T).self) { group in
+			var iterator = enumerated().makeIterator()
+			func addNextTask() {
+				if let next = iterator.next() {
+					group.addTask { (next.offset, try await transform(next.element)) }
+				}
+			}
+
+			for _ in 0..<Swift.min(count, maxConcurrentTaskCount) {
+				addNextTask()
+			}
+
+			return try await group.reduce(into: [T?](repeating: nil, count: count)) { results, indexedResult in
+				results[indexedResult.index] = .some(indexedResult.result)
+				addNextTask()
+			}
+			.map { $0! } // swiftlint:disable:this force_unwrapping
+		}
+	}
+
+	private func concurrentCompactTransform<T: Sendable>(
 		maxConcurrentTaskCount: Int,
 		_ transform: @escaping @Sendable (Element) async throws -> T?,
 	) async rethrows -> [T] {
